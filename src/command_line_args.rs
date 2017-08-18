ee46333457f5033d14414c93654849f8ef3bf2a4
blazern <blazer007@yandex.ru>
2017-08-18 12:30:41 +0700

    Add command line arguments parsing

---
+++ src/command_line_args.rs
@@ -0,0 +1,39 @@
+extern crate docopt;
+
+use std;
+use self::docopt::Docopt;
+
+const USAGE: &'static str = "
+Recipe calculator server.
+
+Usage:
+  recipe_calculator_server <config-path>
+  recipe_calculator_server (-h | --help)
+
+Options:
+  -h --help     Show this screen.
+";
+
+#[derive(Debug, Deserialize)]
+pub struct Args {
+    arg_config_path: String,
+}
+
+impl Args {
+    pub fn config_path(&self) -> &String {
+        &self.arg_config_path
+    }
+}
+
+pub fn get() -> Result<Args, docopt::Error> {
+    return parse(std::env::args());
+}
+
+pub fn parse<I, S>(args: I) -> Result<Args, docopt::Error>
+            where I: IntoIterator<Item=S>, S: AsRef<str>{
+    let args: Result<Args, docopt::Error> =
+        Docopt::new(USAGE)?
+            .argv(args)
+            .deserialize();
+    return args;
+}
