3072c23360113f509d41ded1c4a1b348edc2748f
blazern <blazer007@yandex.ru>
2017-11-09 12:56:57 +0700

    Add GRANT privileges to 'create_app_user' Diesel migration

--- migrations/2017-09-18-053447_create_app_user/up.sql
+++ migrations/2017-09-18-053447_create_app_user/up.sql
@@ -1,1 +1,6 @@
-CREATE TABLE app_user (id SERIAL PRIMARY KEY, uid UUID UNIQUE NOT NULL, vk_uid INTEGER UNIQUE NOT NULL)
+CREATE TABLE app_user (id SERIAL PRIMARY KEY, uid UUID UNIQUE NOT NULL, vk_uid INTEGER UNIQUE NOT NULL);
+
+GRANT SELECT ON TABLE app_user TO recipe_calculator_client;
+GRANT INSERT ON TABLE app_user TO recipe_calculator_client;
+GRANT SELECT ON TABLE app_user_id_seq TO recipe_calculator_client;
+GRANT UPDATE ON TABLE app_user_id_seq TO recipe_calculator_client;
