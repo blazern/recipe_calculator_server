3939dbd7046db3ff085f148906ede929ea0e724f
blazern <blazer007@yandex.ru>
2017-11-12 15:11:04 +0700

    Allow multiple Devices to references single AppUser

--- migrations/2017-11-05-170510_create_vk_user_and_device/up.sql
+++ migrations/2017-11-05-170510_create_vk_user_and_device/up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE device (id SERIAL PRIMARY KEY, uuid UUID UNIQUE NOT NULL, app_user_id INTEGER UNIQUE NOT NULL REFERENCES app_user(id));
+CREATE TABLE device (id SERIAL PRIMARY KEY, uuid UUID UNIQUE NOT NULL, app_user_id INTEGER NOT NULL REFERENCES app_user(id));
 
 CREATE TABLE vk_user (id SERIAL PRIMARY KEY, vk_uid INTEGER UNIQUE NOT NULL, app_user_id INTEGER UNIQUE NOT NULL REFERENCES app_user(id));
 INSERT INTO vk_user(vk_uid, app_user_id) SELECT vk_uid, id FROM app_user;
--- src/db/device_test.rs
+++ src/db/device_test.rs
@@ -19,38 +19,54 @@ include!("../testing_config.rs.inc");
 include!("psql_admin_url.rs.inc");
 
 // Cleaning up before tests
-fn delete_entry_with(uuid: &Uuid) {
+fn delete_entries_with(uuids: &[&Uuid]) {
     let psql_admin_url = env::var(PSQL_ADMIN_URL).unwrap();
     let pg_connection = PgConnection::establish(&psql_admin_url).unwrap();
 
     pg_connection.transaction::<_, error::Error, _>(|| {
-        // Memorize existing entry
-        let existing_entry = select_entry_with(&uuid, &pg_connection);
+        // Memorize existing entries
+        let mut existing_entries = Vec::new();
+        for uuid in uuids {
+            let selected_entry = select_entry_with(&uuid, &pg_connection);
+            match selected_entry {
+                Some(entry) => {
+                    existing_entries.push(entry);
+                }
+                _ => {}
+            }
+        }
 
-        // Delete it
-        delete_by_column!(
+        // Delete them
+        for uuid in uuids {
+            delete_by_column!(
                     schema::device::table,
                     schema::device::uuid,
-                    uuid,
+                    *uuid,
                     &pg_connection)?;
+        }
 
-        // Delete its AppUser (so that foreign key constraint would stop us).
-        match existing_entry {
-            Some(entry) => {
-                delete_by_column!(
+        // Delete their AppUsers (so that foreign key constraint wouldn't stop us).
+        for entry in existing_entries {
+            delete_by_column!(
                     schema::app_user::table,
                     schema::app_user::id,
                     entry.app_user_id(),
                     &pg_connection)?;
-            }
-            _ => {}
         }
 
-        let deleted_entry = select_entry_with(&uuid, &pg_connection);
-        assert!(deleted_entry.is_none());
+
+        for uuid in uuids {
+            let deleted_entry = select_entry_with(&uuid, &pg_connection);
+            assert!(deleted_entry.is_none());
+        }
 
         Ok(())
     }).unwrap();
+}
+
+
+fn delete_entry_with(uuid: &Uuid) {
+    delete_entries_with(&[uuid])
 }
 
 fn select_entry_with(uuid: &Uuid, pg_connection: &PgConnection) -> Option<device::Device> {
@@ -112,6 +128,30 @@ fn cant_insert_device_with_already_used_uuid() {
 
         let second_insertion_result = device::insert(device_copy2, &pg_connection);
         assert!(second_insertion_result.is_err());
+
+        Ok(())
+    }).unwrap();
+}
+
+#[test]
+fn multiple_devices_can_depend_on_single_app_user() {
+    let uuid1 = Uuid::from_str("550e8400-e29b-41d4-a716-446655440002").unwrap();
+    let uuid2 = Uuid::from_str("550e8400-e29b-41d4-a716-446655440003").unwrap();
+    delete_entries_with(&[&uuid1, &uuid2]);
+
+    let config = get_testing_config();
+    let pg_connection = PgConnection::establish(config.psql_diesel_url_client_user()).unwrap();
+
+    // Need to revert insertions if test fails
+    pg_connection.transaction::<_, error::Error, _>(|| {
+        let app_user_uid = Uuid::from_str("550e8400-e29b-41d4-a716-a46655440002").unwrap();
+        let app_user = app_user::insert(app_user::new(app_user_uid), &pg_connection).unwrap();
+
+        let device1 = device::new(uuid1, &app_user);
+        let device2 = device::new(uuid2, &app_user);
+
+        device::insert(device1, &pg_connection).unwrap();
+        device::insert(device2, &pg_connection).unwrap();
 
         Ok(())
     }).unwrap();
