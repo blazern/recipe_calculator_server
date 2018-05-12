9529831da53182da9b759806043bf45185c5b9e8
blazern <blazer007@yandex.ru>
2018-05-12 12:17:03 +0700

    Use RAII in connection_pool and make it thread-safe

--- src/db/pool/connection_pool.rs
+++ src/db/pool/connection_pool.rs
@@ -3,7 +3,13 @@ use db::core::connection::DBConnection;
 use db::core::error::Error as DBCoreError;
 use super::error::Error;
 
-// Primitive connection pool for connections reusing.
+use std::ops::Deref;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+// Thread-safe connection pool for connections reusing.
+
+type WrappedPool = Arc<Mutex<Vec<DBConnection>>>;
 
 pub enum ConnectionType {
     UserConnection,
@@ -13,19 +19,28 @@ pub enum ConnectionType {
 pub struct ConnectionPool {
     connection_type: ConnectionType,
     config: Config,
-    connections: Vec<DBConnection>,
+    connections: WrappedPool,
+}
+
+pub struct BorrowedDBConnection {
+    connection: Option<DBConnection>,
+    connections: WrappedPool,
 }
 
 impl ConnectionPool {
     pub fn new(connection_type: ConnectionType, config: Config) -> Self {
-        ConnectionPool { connection_type, config, connections: Vec::new() }
+        ConnectionPool {
+            connection_type,
+            config,
+            connections: Arc::new(Mutex::new(Vec::new()))
+        }
     }
 
     pub fn for_client_user(config: Config) -> Self {
         return ConnectionPool {
             connection_type: ConnectionType::UserConnection,
             config,
-            connections: Vec::new()
+            connections: Arc::new(Mutex::new(Vec::new()))
         };
     }
 
@@ -33,19 +48,33 @@ impl ConnectionPool {
         return ConnectionPool {
             connection_type: ConnectionType::ServerConnection,
             config,
-            connections: Vec::new()
+            connections: Arc::new(Mutex::new(Vec::new()))
         };
     }
 
-    pub fn borrow(&mut self) -> Result<DBConnection, Error> {
-        match self.connections.pop() {
+    pub fn borrow(&mut self) -> Result<BorrowedDBConnection, Error> {
+        let connection: Option<DBConnection>;
+        {
+            let mut connections = self.connections.lock().unwrap();
+            connection = (&mut connections).pop();
+        }
+        match connection {
             Some(connection) => {
-                return Ok(connection);
+                return Ok(self.connection_to_borrowed(connection));
             }
             None => {
-                return Ok(self.new_connection()?);
+                let new_connection = self.new_connection()?;
+                return Ok(self.connection_to_borrowed(new_connection));
             }
         }
+    }
+
+    fn connection_to_borrowed(&mut self, connection: DBConnection) -> BorrowedDBConnection {
+        let result = BorrowedDBConnection {
+            connection: Some(connection),
+            connections: self.connections.clone()
+        };
+        return result;
     }
 
     fn new_connection(&self) -> Result<DBConnection, DBCoreError> {
@@ -59,12 +88,25 @@ impl ConnectionPool {
         }
     }
 
-    pub fn put_back(&mut self, connection: DBConnection) {
-        self.connections.push(connection);
-    }
-
     pub fn pooled_connections_count(&self) -> usize {
-        return self.connections.len();
+        let connections = self.connections.lock().unwrap();
+        return (&connections).len();
+    }
+}
+
+impl Drop for BorrowedDBConnection {
+    fn drop(&mut self) {
+        let mut connections = self.connections.lock().unwrap();
+        (&mut connections).push(self.connection.take().expect(
+            "Connection expected to be moved out only in drop"));
+    }
+}
+
+impl Deref for BorrowedDBConnection {
+    type Target = DBConnection;
+
+    fn deref(&self) -> &DBConnection {
+        &self.connection.as_ref().expect("Connection expected to be moved out only in drop")
     }
 }
 
--- src/db/pool/connection_pool_test.rs
+++ src/db/pool/connection_pool_test.rs
@@ -7,8 +7,7 @@ fn can_borrow_and_put_back() {
 
     let mut pool = ConnectionPool::for_client_user(config);
 
-    let connection = pool.borrow().unwrap();
-    pool.put_back(connection);
+    let _connection = pool.borrow().unwrap();
 }
 
 #[test]
@@ -18,15 +17,16 @@ fn connections_are_reused() {
     let mut pool = ConnectionPool::for_client_user(config);
     let initial_connections_count = pool.pooled_connections_count();
 
-    let connection1 = pool.borrow().unwrap();
-    assert_eq!(initial_connections_count, pool.pooled_connections_count());
-
-    pool.put_back(connection1);
+    {
+        let _connection1 = pool.borrow().unwrap();
+        assert_eq!(initial_connections_count, pool.pooled_connections_count());
+    }
     assert_eq!(initial_connections_count + 1, pool.pooled_connections_count());
 
-    let connection2 = pool.borrow().unwrap();
-    assert_eq!(initial_connections_count, pool.pooled_connections_count());
+    {
+        let _connection2 = pool.borrow().unwrap();
+        assert_eq!(initial_connections_count, pool.pooled_connections_count());
+    }
 
-    pool.put_back(connection2);
     assert_eq!(initial_connections_count + 1, pool.pooled_connections_count());
 }
